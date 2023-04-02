1ba40f4bdbdc8488542d6bbe8d87629120cec2d7
hungnv198 <127464412+hungnv198@users.noreply.github.com>
2023-04-02 13:32:11 +0700

    up

--- main.js
+++ main.js
@@ -104,6 +104,7 @@ headingElement.className = 'heading';
 //Lấy trong DOM
 //console.log(boxElement.innerHTML);
 //--------------Ví dụ:
+/*
 var courses = ['HTML & CSS', 'Javascript', 'PHP', 'Java']
 //Tạo hàm thêm element node vào 1 node có sẵn từ 1 mảng cho trc
 function render(courses) {
@@ -122,3 +123,206 @@ function render(courses) {
 console.log(render(courses));
 
 // render(courses)
+*/
+//-----------------------------------------------------
+//----------------Node property----------------------
+// var boxElement = document.querySelector('.box');
+// console.log([boxElement]);
+//Tìm thuộc tính của element:
+//thuộc tinh cần chú ý:
+/*
+- attributes: cũng là một đối tượng và chứa nhiều thuộc tính bên trong
+- contenteditable: làm cho 1 div trở thành thẻ input.
+- first-child:
+- id
+- tabindex
+*/
+//-----------------DOM CSS----------------------------
+//--------DOM style-----------
+/*
+var boxElement = document.querySelector('.box');
+console.log(boxElement.style);
+//---Viết thường từ đầu tiên, viết hoa từ tiếp theo: camelCase
+// boxElement.style.width = '100px';
+// boxElement.style.height = '100px';
+// boxElement.style.backgroundColor = 'red';
+
+//Sử dụng đối tượng Object
+Object.assign(boxElement.style, {
+    width: '200px',
+    height: '200px',
+    backgroundColor: 'green',
+});// Set CSS inline
+
+//in ra thuộc tính:
+console.log(boxElement.style.backgroundColor);
+*/
+//------------------------------------------------------------
+//--------------------classList property---------------------
+// var boxElement = document.querySelector('.box');
+// console.log(boxElement.classList);
+//in ra DOMTokenList: quản lí Class của element đang được gọi
+//add, contains, remove, toggle:nếu có: xóa bỏ, không có: thêm
+//
+//boxElement.classList.add('red');
+//Thêm nhiều class, mỗi class cách nhau bằng 1 dấu phẩy
+//boxElement.classList.add('red','blue');
+//Kiểm tra class có tồn tại trong element hay ko
+//console.log(boxElement.classList.contains('red'));
+//Xóa một class trong classlist element
+// setTimeout(function(){
+// boxElement.classList.remove('red');
+// }, 3000);
+//toggle: 
+// setTimeout(function(){
+//     boxElement.classList.toggle('red');
+//     }, 3000);
+//Tạo ví dụ chữ nhấp nháy:
+// setInterval(function(){
+//     boxElement.classList.toggle('red');
+// }, 1000);
+//-----------------------DOM Events---------------------
+//----------------------------------------------------
+//1. Attribute events:
+//2. Assign event by using element node
+//Cần biết đc tên events:
+//Search: DOM EVENT
+//Thường sử dụng: click, resize, scroll
+//---------Event Click
+//thêm tiền tố: on trước attribute
+//click vào thẻ con của dom-even: vẫn lấy thẻ chứa on-click
+//----------------Gán event ----------
+//get element:
+// var h1Element = document.querySelectorAll('h1');
+// console.log(h1Element);
+// //chạy vòng lặp để click vào từng tag h1
+// for(var i = 0; i < h1Element.length; i++){
+//     h1Element[i].onclick = function(event){
+//         //sử dụng event để trả về sự kiện người dùng click
+//         console.log(event.target);
+//     }
+// }
+//Gọi onclick và gán cho 1 function
+// h1Element.onclick = function(e){
+//     //lấy nội dung của chính element
+//     //trả lại 1 attribute là target:
+//     console.log(e.target.innerText);
+// }
+//-----------------------------DOM Events Example--------------
+// sd element node
+//var inputValue;
+//var inputElement = document.querySelector('input[type="text"]');
+//console.log(inputElement);
+//Lắng nghe sự kiện
+/*
+//onchange chạy khi khác ndung ban đầu và click ra ngoài
+inputElement.onchange = function(event) {
+    console.log(event.target.value);
+}
+//oninput chạy ngay khi nội dung khác , đang được gõ
+inputElement.oninput = function(event) {
+    
+    inputValue = event.target.value;
+    //console.log(event.target.value);
+}
+//check box:
+var checkElement = document.querySelector('input[type="checkbox"]');
+checkElement.onchange = function(event) {
+    //dùng checked để xem có đang check
+    //console.log(event.target.checked);
+}
+//Select
+var selectElement = document.querySelector('select');
+selectElement.onchange = function(event) {
+    //in ra giá trị value của thẻ,
+    console.log(event.target.value);
+}
+*/
+//Key up:
+// var inputElement = document.querySelector('input[type="text"]');
+// inputElement.onkeyup = function(event){
+//     //console.log(event.target.value);
+//     //trả ra mã phím bấm
+//     console.log(event.which);
+//     switch(event.which){
+//         case 27:
+//             console.log('exit');
+//             break
+//     }
+// }
+//Thực thi bên ngoài
+// document.onkeydown = function(event){
+//     switch(event.which){
+//         case 27:
+//             console.log('exit');
+//             break
+//     }
+// }
+
+//-----------------------------PreventDefault vaf stopPropagation
+
+// var aElements = document.querySelectorAll('a');
+// //dùng anchors phải đặt name
+// //console.log(aElements);
+// //lắng nghe sự kiện
+// for(var i = 0; i < aElements.length; i++){
+//     aElements[i].onclick = function(event){
+//         //console.log(event.target.href);
+//         if(!event.target.href.startsWith('https://gamek.vn')){
+//             //ngăn chặn hành vi mặc định
+//             event.preventDefault();
+
+//         }
+//     }
+// }
+//Bắt sự kiện thẻ li
+// var ulElement = document.querySelector('ul');
+// ulElement.onmousedown = function(event){
+//     //bỏ đi hành vi mặc định của đối tượng
+//     event.preventDefault();
+// }
+// ulElement.onclick = function(event) {
+//     console.log(event.target);
+// }
+// var divElement = document.querySelector('div')
+// divElement.onclick = function(event){
+//     console.log("DIV");
+// }
+// var buttonElement = document.querySelector('button');
+// buttonElement.onclick = function(event){
+//     //Ngăn chặn nổi bọt
+//     event.stopPropagation();
+//     console.log("Click Me!");
+// }
+//----------------Even Listener----------------------
+
+var btn = document.querySelector('button');
+//xử lý nhiều việc  khi event xảy ra
+// setTimeout(function(){
+//     btn.onclick = function(event){
+//         console.log('Viec 1');
+        
+//         console.log('Viec 2');
+    
+//         alert("Việc 3")
+    
+//     }
+// }, 3000);
+//btn là 1 object, nên có thể ghi đè
+//lắng nghe/ hủy bỏ lắng nghe
+//Lắng nghe sự kiện
+function viec1(){
+    console.log("viec_1");
+}
+btn.addEventListener('click', viec1);//2 đối số: eventname, functioncallback
+btn.addEventListener('click', function(event){
+    console.log('event 2');
+});//2 đối số: eventname, functioncallback
+btn.addEventListener('click', function(event){
+    console.log('event 3');
+});//2 đối số: eventname, functioncallback
+//hủy bỏ lắng nghe
+setTimeout(function(){
+    btn.removeEventListener('click', viec1);
+}, 3000);
+// sd eventlister: ko muốn lắng nghe liên tục, hủy bỏ lắng nghe
